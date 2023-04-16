36d12eeb31e53ce9c2422383cf1e4f52209fc047
SadiqSalam <125434127+SadiqSalam@users.noreply.github.com>
2023-04-16 14:28:38 +0800

    Create number_game.h

---
+++ number_game.h
@@ -0,0 +1,13 @@
+#ifndef NUMBER_GAME_H
+#define NUMBER_GAME_H
+
+using namespace std;
+
+void stage1(int array[]);   //function for stage 1 which inputs an array
+void stage2(int array[],int key1);  //function for stage 1 which inputs an array and the first key
+void stage3(int array[],int key1,int key2);   //function for stage 1 which inputs an array, the first and the second key
+void stage4(int array[],int key1,int key2, int key3);   //function for stage 1 which inputs an array, the first, the second and the third key
+void stage5(int array[],int key1, int key2, int key3, int key4);  //function for stage 1 which inputs an array, the first, the second, the third and the fourth key
+void printarray(int arr[]);
+
+#endif
