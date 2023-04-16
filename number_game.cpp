19a16906c401672f506df32f8c92a40f52e31171
SadiqSalam <125434127+SadiqSalam@users.noreply.github.com>
2023-04-16 14:05:04 +0800

    Update number_game.cpp

--- number_game.cpp
+++ number_game.cpp
@@ -2,7 +2,6 @@
 #include <array>
 #include <cstdlib>
 using namespace std;
-
 void stage1(int array[]);
 void stage2(int array[],int key1);
 void stage3(int array[],int key1,int key2);
@@ -11,7 +10,7 @@ void stage5(int array[],int key1, int key2, int key3, int key4);
 
 
 void printarray(int arr[]){
-  for (int x=0; x < 4; x++){
+	for (int x=0; x < 4; x++){
 		cout << arr[x] << ""; }
 	cout << endl;
 
@@ -23,25 +22,25 @@ void stage1(int array[]){
 	cout << "Stage 1: " <<  i << endl;
 	int check;
 	if (i==1){
-						cin >> check;
-						if (check == array[1])
-							stage2(array,check);	
-						}
+		cin >> check;
+		if (check == array[1])
+			stage2(array,array[1]);	
+	}
 	if (i==2){
-						cin >> check;
-						if (check == array[1])
-							stage2(array,check);
-					}
+		cin >> check;
+		if (check == array[1])
+			stage2(array,array[1]);
+	}
 	if (i==3){
-						cin >>  check;
-						if (check == array[2])
-							stage2(array,check);
-					}
+		cin >>  check;
+		if (check == array[2])
+			stage2(array,array[2]);
+	}
 	if (i==4){
-						cin >>  check;
-						if (check == array[3])
-							stage2(array,check);
-					}
+		cin >>  check;
+		if (check == array[3])
+			stage2(array,array[3]);
+	}
 			
 }
 
@@ -51,24 +50,24 @@ void stage2(int array[],int key1){
 	int i = (rand() % 4) + 1;
 	cout << "Stage 2: " << i << endl;
 	if (i==1){
-           			  cin >>  check;
+                  cin >>  check;
                   if (check == 4)
-                          stage3(array,key1,check);
+                          stage3(array,key1,4);
         }
         if (i==2){
                   cin >>  check;
                   if (check == key1)
-                          stage3(array,key1,check);
+                          stage3(array,key1,key1);
         }
         if (i==3){
                   cin >>  check;
                   if (check == array[0])
-                          stage3(array,key1,check);
+                          stage3(array,key1,array[0]);
         }
         if (i==4){
                   cin >>  check;
                   if (check == key1)
-                          stage3(array,key1,check);
+                          stage3(array,key1,key1);
 	}
 }
 
@@ -80,56 +79,56 @@ void stage3(int array[],int key1,int key2){
           if (i==1){
                     cin >>  check;
                     if (check == key2)
-                            stage4(array,key1,key2,check);
+                            stage4(array,key1,key2,key2);
           }
           if (i==2){
                     cin >> check;
                     if (check == key1)
-                            stage4(array,key1,key2,check);
+                            stage4(array,key1,key2,key1);
           }
           if (i==3){
                     cin >>  check;
                     if (check == array[2])
-                            stage4(array,key1,key2,check);
+                            stage4(array,key1,key2,array[2]);
           }
           if (i==4){
                     cin >>  check;
                     if (check == 4)
-                            stage4(array,key1,key2,check);
+                            stage4(array,key1,key2,4);
           }
 }
 
 void stage4(int array[],int key1,int key2, int key3){
-      srand(time(0));
+            srand(time(0));
 	    int check;
-      int i = (rand() % 4) + 1;
+            int i = (rand() % 4) + 1;
 	    cout << "Stage 4: " << i << endl;
-      if (i==1){
+            if (i==1){
                       cin >>  check;
                       if (check == key1)
-                              stage5(array,key1,key2,key3,check);
-            		}
-      if (i==2){
+                              stage5(array,key1,key2,key3,key1);
+            }
+            if (i==2){
                       cin >>  check;
                       if (check == array[0])
-                              stage5(array,key1,key2,key3,check);
-            		}
-      if (i==3){
-                      cin >>  check;
-                      if (check == key2)
-                              stage5(array,key1,key2,key3,check);
+                              stage5(array,key1,key2,key3,array[0]);
             }
-      if (i==4){
+            if (i==3){
                       cin >>  check;
                       if (check == key2)
-                              stage5(array,key1,key2,key3,check);
-            		}
+                              stage5(array,key1,key2,key3,key2);
+            }
+            if (i==4){
+                      cin >>  check;
+                      if (check == key2)
+                              stage5(array,key1,key2,key3,key2);
+            }
 }
 
 void stage5(int array[],int key1,int key2, int key3, int key4){
-        srand(time(0));
+              srand(time(0));
 	      int check;
-        int i = (rand() % 4) + 1;
+              int i = (rand() % 4) + 1;
 	      cout << "Stage 5: " <<i << endl;
               if (i==1){
                         cin >> check;
@@ -139,10 +138,9 @@ void stage5(int array[],int key1,int key2, int key3, int key4){
               if (i==2){
                         cin >> check;
                         if (check == key2)
- 																cout << "Congratulations! You solved the puzzle!" << endl;             
-	     				}	
-             
-							if (i==3){
+ 				cout << "Congratulations! You solved the puzzle!" << endl;             
+	     }	
+             if (i==3){
                        cin >> check;
                        if (check == key4)
                                cout << "Congratulations! You solved the puzzle!" << endl;
@@ -156,18 +154,18 @@ void stage5(int array[],int key1,int key2, int key3, int key4){
 int main(){
 	cout << "Welcome to the memory game!" << endl;
 	int array[4];
-  bool temp = true;
-  int x = 0;
+        bool temp = true;
+        int x = 0;
 	srand(time(0));
         while (x < 4){
         	int i =  (rand() % 4) + 1 ;
-          if (array[0]!=i && array[1]!=i && array[2]!=i && array[3]!=i){
-						array[x] = i;
-						x++; }
+                if (array[0]!=i && array[1]!=i && array[2]!=i && array[3]!=i){
+			array[x] = i;
+			x++; }
   
  }
 
 	printarray(array);
- 	stage1(array);
+  	stage1(array);
 
 }
