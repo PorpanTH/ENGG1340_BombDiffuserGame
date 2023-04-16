0ff066698e66a195eb49e889b7a3fd4c8fe1be49
SadiqSalam <125434127+SadiqSalam@users.noreply.github.com>
2023-04-16 15:13:31 +0800

    Update number_game.cpp

--- number_game.cpp
+++ number_game.cpp
@@ -2,13 +2,12 @@
 #include <array>
 #include <cstdlib>
 #include "number_game.h"
-
 using namespace std;
 
 void printarray(int arr[]){
-	for (int x=0; x < 4; x++){
-		cout << arr[x] << " "; }
-	cout << endl;
+	cout << "┏━━━┓┏━━━┓┏━━━┓┏━━━┓" << endl;
+	cout << "┃" << " " << arr[0] << " " << "┃" << "┃" << " " << arr[1] << " " << "┃" << "┃" << " " << arr[2] << " " << "┃"  << "┃" << " " << arr[3] << " " << "┃" << endl;
+	cout << "┗━━━┛┗━━━┛┗━━━┛┗━━━┛" << endl;
 
 }
 
@@ -19,21 +18,33 @@ void stage1(int array[]){
 	int check;
 	if (i==1){
 		cin >> check;
+		while (check != array[1]){
+			cout << "Wrong Input. Try Again!" << endl;
+			cin >> check; }
 		if (check == array[1])
-			stage2(array,array[1]);	
+			stage2(array,array[1]);
 	}
 	if (i==2){
 		cin >> check;
+		while (check != array[1]){
+                          cout << "Wrong Input. Try Again!" << endl;
+                          cin >> check; }
 		if (check == array[1])
 			stage2(array,array[1]);
 	}
 	if (i==3){
 		cin >>  check;
+		while (check != array[2]){
+                          cout << "Wrong Input. Try Again!" << endl;
+                          cin >> check; }
 		if (check == array[2])
 			stage2(array,array[2]);
 	}
 	if (i==4){
 		cin >>  check;
+		while (check != array[3]){
+                          cout << "Wrong Input. Try Again!" << endl;
+                          cin >> check; }
 		if (check == array[3])
 			stage2(array,array[3]);
 	}
@@ -41,108 +52,168 @@ void stage1(int array[]){
 }
 
 void stage2(int array[],int key1){
+	for (int i=0; i<100;i++){
+		cout << endl; }
+	printarray(array);
 	srand(time(0));
 	int check;
 	int i = (rand() % 4) + 1;
 	cout << "Stage 2: " << i << endl;
 	if (i==1){
                   cin >>  check;
+		  while (check != 4){
+                          cout << "Wrong Input. Try Again!" << endl;
+                          cin >> check; }
                   if (check == 4)
                           stage3(array,key1,4);
         }
         if (i==2){
                   cin >>  check;
+		  while (check != key1){
+                            cout << "Wrong Input. Try Again!" << endl;
+                            cin >> check; }
                   if (check == key1)
                           stage3(array,key1,key1);
         }
         if (i==3){
-                  cin >>  check;
+                  cin >> check;
+		  while (check != array[0]){
+                            cout << "Wrong Input. Try Again!" << endl;
+                            cin >> check; }
                   if (check == array[0])
                           stage3(array,key1,array[0]);
         }
         if (i==4){
-                  cin >>  check;
+                  cin >> check;
+		  while (check != key1){
+                            cout << "Wrong Input. Try Again!" << endl;
+                            cin >> check; }
                   if (check == key1)
                           stage3(array,key1,key1);
 	}
 }
 
 void stage3(int array[],int key1,int key2){
-          srand(time(0));
+          for (int i=0; i<100;i++){
+                  cout << endl; }
+          printarray(array);
+	  srand(time(0));
 	  int check;
           int i = (rand() % 4) + 1;
 	  cout << "Stage 3: " << i << endl;
           if (i==1){
                     cin >>  check;
+		    while (check != key2){
+                            cout << "Wrong Input. Try Again!" << endl;
+                            cin >> check; }
                     if (check == key2)
                             stage4(array,key1,key2,key2);
           }
           if (i==2){
                     cin >> check;
+		    while (check != key1){
+                            cout << "Wrong Input. Try Again!" << endl;
+                            cin >> check; }
                     if (check == key1)
                             stage4(array,key1,key2,key1);
           }
           if (i==3){
                     cin >>  check;
+		    while (check != array[2]){
+                            cout << "Wrong Input. Try Again!" << endl;
+                            cin >> check; }
                     if (check == array[2])
                             stage4(array,key1,key2,array[2]);
           }
           if (i==4){
                     cin >>  check;
+		    while (check != 4){
+                            cout << "Wrong Input. Try Again!" << endl;
+                            cin >> check; }
                     if (check == 4)
                             stage4(array,key1,key2,4);
           }
 }
 
 void stage4(int array[],int key1,int key2, int key3){
-            srand(time(0));
+            for (int i=0; i<100;i++){
+                  cout << endl; }
+            printarray(array);
+	    srand(time(0));
 	    int check;
             int i = (rand() % 4) + 1;
 	    cout << "Stage 4: " << i << endl;
             if (i==1){
-                      cin >>  check;
+                      cin >> check;
+		      while (check != key1){
+                            cout << "Wrong Input. Try Again!" << endl;
+                            cin >> check; }
                       if (check == key1)
                               stage5(array,key1,key2,key3,key1);
             }
             if (i==2){
                       cin >>  check;
+		      while (check != array[0]){
+                            cout << "Wrong Input. Try Again!" << endl;
+                            cin >> check; }
                       if (check == array[0])
                               stage5(array,key1,key2,key3,array[0]);
             }
             if (i==3){
-                      cin >>  check;
+                      cin >> check;
+		      while (check != key2){
+                            cout << "Wrong Input. Try Again!" << endl;
+                            cin >> check; }
                       if (check == key2)
                               stage5(array,key1,key2,key3,key2);
             }
             if (i==4){
-                      cin >>  check;
+                      cin >> check;
+		      while (check != key2){
+                            cout << "Wrong Input. Try Again!" << endl;
+                            cin >> check; }
                       if (check == key2)
                               stage5(array,key1,key2,key3,key2);
             }
 }
 
 void stage5(int array[],int key1,int key2, int key3, int key4){
-              srand(time(0));
+              for (int i=0; i<100;i++){
+                  cout << endl; }
+              printarray(array);
+	      srand(time(0));
 	      int check;
               int i = (rand() % 4) + 1;
 	      cout << "Stage 5: " <<i << endl;
               if (i==1){
                         cin >> check;
+			while (check != key1){
+                            cout << "Wrong Input. Try Again!" << endl;
+                            cin >> check; }
                         if (check == key1)
                                 cout << "Congratulations! You solved the puzzle!" << endl;
               }
               if (i==2){
                         cin >> check;
+			while (check != key2){
+                            cout << "Wrong Input. Try Again!" << endl;
+                            cin >> check; }
                         if (check == key2)
  				cout << "Congratulations! You solved the puzzle!" << endl;             
 	     }	
              if (i==3){
                        cin >> check;
+		       while (check != key4){
+                            cout << "Wrong Input. Try Again!" << endl;
+                            cin >> check; }
                        if (check == key4)
                                cout << "Congratulations! You solved the puzzle!" << endl;
              }
              if (i==4){
                        cin >> check;
+		       while (check != key3){
+                            cout << "Wrong Input. Try Again!" << endl;
+                            cin >> check; }
                        if (check == key3)
                                cout << "Congratulations! You solved the puzzle!" << endl;
              }
@@ -159,9 +230,9 @@ int main(){
 			array[x] = i;
 			x++; }
   
- }
+ 	}
 
 	printarray(array);
   	stage1(array);
-
+	
 }
