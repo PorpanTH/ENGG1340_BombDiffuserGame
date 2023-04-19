a436cbcf202f4a0a171da86855fc585a4f254e9c
PorpanTH <60740681+PorpanTH@users.noreply.github.com>
2023-04-19 14:25:20 +0800

    updated word_game

--- word_game.cpp
+++ word_game.cpp
@@ -4,17 +4,17 @@
 #include <cstdlib>
 #include <algorithm>
 #include "word_game.h"
-
+#include "time.h"
 using namespace std;
 
 int select_main_word(string words[6][15], string &main_word){
     int random = rand()%5; //generate a random number between 0 and 5
     main_word = words[random][0]; //use the random number to select a random word from column 1 of the 2d array
-    return random; // return the word. this will be the "main_word". 
+    return random; // return the word. this will be the "main_word".
 } //the index of the main_word is returned as main_index, to makeit easier to code
-    
+
 void select_words(string words[6][15], string &word1, string &word2, string &word3, string &word4, string &word5, string &word6){
-    int a[6] = {0,1,2,3,4,5}; 
+    int a[6] = {0,1,2,3,4,5};
     int b[14] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14}; //where elements from a and b are in the form words[a][b]
     random_shuffle(a, a + 6); //the two arrays "a","b" with the indices are randomly shuffled
     random_shuffle(b, b + 13);
@@ -24,7 +24,7 @@ void select_words(string words[6][15], string &word1, string &word2, string &wor
     word4 = words[a[3]][b[3]];
     word5 = words[a[4]][b[4]];
     word6 = words[a[5]][b[5]];
-    
+
 }
 
 void print_ui(string main_word, string word1, string word2, string word3, string word4, string word5, string word6){
@@ -39,13 +39,13 @@ void print_ui(string main_word, string word1, string word2, string word3, string
     cout << "┗━━━━━━━┛┗━━━━━━━┛" << endl;
     cout << "┏━━━━━━━┓┏━━━━━━━┓" << endl;
     cout << "┃" << " " << word5 << " " << "┃" << "┃" << " " << word6 << " " << "┃" << endl;
-    cout << "┗━━━━━━━┛┗━━━━━━━┛" << endl;    
+    cout << "┗━━━━━━━┛┗━━━━━━━┛" << endl;
 }
 
 
 string find_word(string words[6][15], int main_index, string main_word, string word1, string word2, string word3, string word4, string word5, string word6){
     string answer; //this function is used to find the correct answer for the puzzle. the correct answer is then stored in the variable answer
-    for (int i = 0; i < 15; i++){ //main_index allows to directly access the correct row from where      
+    for (int i = 0; i < 15; i++){ //main_index allows to directly access the correct row from where
         if (words[main_index][i] == word1){ // to loop through every element of the corresponding row
             return word1; //the first word to be matched is immediately returned
         }else if (words[main_index][i] == word2){
@@ -63,35 +63,52 @@ string find_word(string words[6][15], int main_index, string main_word, string w
     return ""; //there was a compilation error, so this statement was added
 }
 
-bool check_word(string answer, string user_word){ 
+bool check_word(string answer, string user_word){
     return (answer == user_word); //this function checks if the user input matches the correct answer
 }
+int word_game(chrono::steady_clock::time_point& end_time){
+    while (chrono::steady_clock::now() < end_time) {
+        //create list of words in an array with 14 columns and 6 rows
+        string words[6][15] = {{"FLUTE", "PAUSE", "SPARK", "SMOKE", "TIMER", "WRONG", "PRESS", "LOOSE", "THINK", "FLUTE", "BURNT", "FIRST", "UHH:(", "HELP!", "WAIT!"},
+                               {"FIRST", "WRONG", "SPARK", "PAUSE", "TIMER", "BURNT", "LOOSE", "HELP!", "UHH:(", "WAIT!", "FLUTE", "THINK", "SMOKE", "PRESS", "FIRST"},
+                               {"BURNT", "THINK", "UHH:(", "WAIT!", "FIRST", "SMOKE", "FLUTE", "LOOSE", "PAUSE", "HELP!", "WRONG", "PRESS", "SPARK", "BURNT", "TIMER"},
+                               {"THINK", "WAIT!", "LOOSE", "SPARK", "TIMER", "THINK", "PRESS", "FLUTE", "HELP!", "BURNT", "SMOKE", "WRONG", "UHH:(", "PAUSE", "FIRST"},
+                               {"THING", "UHH:(", "LOOSE", "SPARK", "TIMER", "PAUSE", "THINK", "BURNT", "PRESS", "WRONG", "SMOKE", "WAIT!", "FIRST", "HELP!", "FLUTE"},
+                               {"PAUSE", "SPARK", "LOOSE", "UHH:(", "TIMER", "FIRST", "SMOKE", "PRESS", "FLUTE", "HELP!", "PAUSE", "WRONG", "THINK", "BURNT", "WAIT!"}};
+        //initialise all the variables
+        string main_word, word1, word2, word3, word4, word5, word6, answer, user_word;
+        int main_index;
 
-int word_game(){
+        srand((unsigned) time(NULL)); //generate a seed
 
-    //create list of words in an array with 14 columns and 6 rows
-   string words[6][15] = {{"FLUTE","PAUSE","SPARK","SMOKE","TIMER","WRONG","PRESS","LOOSE","THINK","FLUTE","BURNT","FIRST","UHH:(","HELP!","WAIT!"},{"FIRST","WRONG","SPARK","PAUSE","TIMER","BURNT","LOOSE","HELP!","UHH:(","WAIT!","FLUTE","THINK","SMOKE","PRESS","FIRST"},{"BURNT","THINK","UHH:(","WAIT!","FIRST","SMOKE","FLUTE","LOOSE","PAUSE","HELP!","WRONG","PRESS","SPARK","BURNT","TIMER"},{"THINK","WAIT!","LOOSE","SPARK","TIMER","THINK","PRESS","FLUTE","HELP!","BURNT","SMOKE","WRONG","UHH:(","PAUSE","FIRST"},{"THING","UHH:(","LOOSE","SPARK","TIMER","PAUSE","THINK","BURNT","PRESS","WRONG","SMOKE","WAIT!","FIRST","HELP!","FLUTE"},{"PAUSE","SPARK","LOOSE","UHH:(","TIMER","FIRST","SMOKE","PRESS","FLUTE","HELP!","PAUSE","WRONG","THINK","BURNT","WAIT!"}};
-    //initialise all the variables
-    string main_word, word1, word2, word3, word4, word5, word6, answer, user_word;
-    int main_index;
+        main_index = select_main_word(words, main_word); //index of the array corresponding to row of main_word
+        select_words(words, word1, word2, word3, word4, word5,
+                     word6); //randomly select 6 words from each row and store in 6 variables
+        answer = find_word(words, main_index, main_word, word1, word2, word3, word4, word5,
+                           word6); //find the correct answer for the puzzle
+        print_ui(main_word, word1, word2, word3, word4, word5, word6); //print the ui
+        cout << "Enter word: "; //prompt for ui
+        cin >> user_word; //store user input in user_word variable
+        cout << endl;
 
-    srand((unsigned) time(NULL)); //generate a seed
+        while (chrono::steady_clock::now() < end_time && !check_word(answer, user_word)) { //keep on looping till user input is correct or till time runs out
+            cout << "Wrong 5 seconds deducted" << endl;
+            end_time -= chrono::seconds(5);
+            auto remaining_time = chrono::duration_cast<chrono::seconds>(end_time - chrono::steady_clock::now());
+            cout << "Time remaining: " << remaining_time.count() << " seconds" << endl;
 
-    main_index = select_main_word(words, main_word); //index of the array corresponding to row of main_word
-    select_words(words, word1, word2, word3, word4, word5, word6); //randomly select 6 words from each row and store in 6 variables
-    answer = find_word(words, main_index, main_word, word1, word2, word3, word4, word5, word6); //find the correct answer for the puzzle
-    print_ui(main_word, word1, word2, word3, word4, word5, word6); //print the ui
-    cout << "Enter word: "; //prompt for ui
-    cin >> user_word; //store user input in user_word variable
-    cout <<  endl;
-    
-    while (!check_word(answer, user_word)){ //keep on looping till user input is correct or till time runs out
-        cout << "Wrong answer! You loose 5 sec :(" << endl;
-        print_ui(main_word, word1, word2, word3, word4, word5, word6);
-        cout << "Enter word: ";
-        cin >> user_word;
-        cout <<  endl;
+            print_ui(main_word, word1, word2, word3, word4, word5, word6);
+            cout << "Enter word: ";
+            cin >> user_word;
+            cout << endl;
+        }
+        if(chrono::steady_clock::now() < end_time){
+
+            cout << "YOU WIN THIS ROUND!" << endl;
+            return 1; //since function return type is int
+        }
     }
-    cout << "YOU WIN THIS ROUND!" << endl; 
-    return 1; //since function return type is int
+
+    return 0;
 }
+
--- word_game.h
+++ word_game.h
@@ -1,47 +1,24 @@
+
 // word_game.h
 #ifndef WORD_GAME_H
 #define WORD_GAME_H
 
 #include <string>
-
+#include "time.h"
 using namespace std;
 
-int select_main_word(string words[6][15], string &main_word);  
-//1)what it does: randomly select the main word from column one
-//2)what the inputs are: 
-//a)words: 2D array holding the words
-//b)main_word: variable to hold the main_word
-//3)what the outputs are: returns an integer that contains the row of main_word. This value is stored in main_index for later use.
+int select_main_word(string words[6][15], string &main_word); //randomly select word from first column of "words" array and assign to variable with reference
 
-void select_words(string words[6][15], string &word1, string &word2, string &word3, string &word4, string &word5, string &word6); 
-//1)what it does: select 6 random words from "words" array and assign them to a variable using a reference
-//2)what the inputs are: 
-//a)words: 2D array holding the words
-//b)word1, word2, word3, word4, word5, word6: to store the 6 randomly selected words
-//3)what the outputs are: no output
+void select_words(string words[6][15], string &word1, string &word2, string &word3, string &word4, string &word5, string &word6); //select 6 random words from "words" array and assign them to a variable using a reference
 
-void print_ui(string main_word, string word1, string word2, string word3, string word4, string word5, string word6); 
-//1)what it does: print the user interface
-//2)what the inputs are: 
-//a)main_word: to store the main word
-//b)word1, word2, word3, word4, word5, word6: to store the 6 randomly selected words
-//3)what the outputs are: no output
+void print_ui(string main_word, string word1, string word2, string word3, string word4, string word5, string word6); //print the user interface
 
-string find_word(string words[6][15], int main_index, string main_word, string word1, string word2, string word3, string word4, string word5, string word6); 
-//1)what it does: find the correct answer
-//2)what the inputs are: 
-//a)main_word, word1, word2, word3, word4, word5, word6: same as earlier
-//b)main_index: contains which row main_word is from
-//3)what the outputs are: correct answer to the puzzle
 
-bool check_word(string answer, string user_word); 
-//1)what it does: check if user input is correct
-//2)what the inputs are: 
-//a)answer: correct answer
-//b)user_word: user input
-//3)what the outputs are: 1 if answer is correct, else 0
+string find_word(string words[6][15], int main_index, string main_word, string word1, string word2, string word3, string word4, string word5, string word6); //find the correct answer for the puzzle
 
-int word_game();
-//main function
+bool check_word(string answer, string user_word); //check if the word entered by the user is correct
+
+int word_game(chrono::steady_clock::time_point& end_time);
+
 
 #endif
